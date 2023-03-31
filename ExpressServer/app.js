6b78f3bc8eeaf9264f147f89d6546f5bb3e16746
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-31 12:39:22 +0530

    learnt how express server works

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -1,20 +1,20 @@
 const express=require('express');
-
+const body=require('body-parser');
+const bodyParser = require('body-parser');
 const expressApp=express();
 
-expressApp.use('/add-product',(req,res,next)=>
+
+const adminRoute=require('./routes/admin');
+const shopRoute=require('./routes/shop');
+expressApp.use(bodyParser.urlencoded({extended:false}));
+
+expressApp.use('/admin',adminRoute);
+expressApp.use(shopRoute);
+
+expressApp.use((req,res,next)=>
 {
-    console.log("In add-product middleware");
-    res.send('<h1>Hello from add-product</h1>');
+    res.status(404).send('<h1>page not found</h1>')
 });
-
-
-expressApp.use('/',(req,res,next)=>
-{
-    console.log("In / middleware");
-    res.send('<h1>Hello from Express</h1>');
-});
-
 // we dont really need this if we are using express
 // const server = http.createServer(expressApp);
 
